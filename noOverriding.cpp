a6f97fd29eabe56ee335d330fe3febc14c011ec8
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-27 10:27:47 +0700

    Memasukan function main

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -14,3 +14,10 @@ class derivedClass : public baseClass {
         cout << "Hallo saya Function dari derived Class ";
     }
 };
+
+int mai () {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
