62c344bb63c26eb75d1e155f8b2105d6a0ac4327
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-27 10:03:18 +0700

    Memasukan class joko dan lia

--- Polimorpi.cpp
+++ Polimorpi.cpp
@@ -5,3 +5,17 @@ class seseorang {
     public:
     virtual void pesan () = 0;
 };
+
+class joko : public seseorang {
+    public:
+    void pesan(){
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia :public seseorang {
+    public:
+    void pesan(){
+        cout << "Pesan dari lia" <<endl;
+    }
+};
