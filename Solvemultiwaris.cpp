e7d7b3d2405ded5c2f2f3c079790ba009bc8d630
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-27 10:47:02 +0700

    Memasukan class pekerja, pelajar, budi

--- Solvemultiwaris.cpp
+++ Solvemultiwaris.cpp
@@ -11,3 +11,32 @@ class orang {
         cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
     }
 };
+
+class pekerja : public orang {
+    public:
+    pekerja (int pUmur) :
+    orang(pUmur)
+    {
+        cout << "Pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : public orang {
+    public:
+    pelajar (int pUmur) :
+    orang(pUmur)
+    {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public orang {
+    public:
+    budi (int pUmur) :
+    orang(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+
